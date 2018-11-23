63b0af134d9b950be4bba6fed002627fcc0cbeca
Sean Fisher <seanfisher@Seans-MacBook-Air.local>
2018-11-23 15:24:09 +0000

    heroku

--- errors/index.js
+++ errors/index.js
@@ -20,5 +20,8 @@ exports.handle422 = (err, req, res, next) => {
 exports.handle500 = (err, req, res, next) => {
   if (err.status) {
     res.status(err.status).send({ msg: err.message });
-  } else { res.status(500).send({ msg: 'Internal Server Error' }); }
+  } else {
+    console.log(err);
+    res.status(500).send({ msg: 'Internal Server Error' });
+  }
 };
